4b539d76f9aa9dcd383dc3a112a6337320aba79a
suceleste <47870191+suceleste@users.noreply.github.com>
2022-02-06 17:29:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-
+// constante avec l'url de l'api
 const APICovid = "https://coronavirusapifr.herokuapp.com/data/live/france";
-
+//utilisation de fetch() sur l'api suivi de then() pour convertir le tout en .jso et aller cherche les data se trouvant dedans
 fetch(APICovid).then(response => response.json().then(data => {
+    //récupération de chaque donné et implémentation dans le html
     let dico = data[0];
     let divt = document.getElementById("Covid-19");
     divt.innerHTML = '<h2>Information Covid-19(France - '+dico.date+' ) :</h2>';
@@ -11,9 +12,11 @@ fetch(APICovid).then(response => response.json().then(data => {
     divt.innerHTML += '<p> Personnes décédé à cause du Covid : '+dico.dc_tot+'</p>'
 }));
 
+// constante avec l'url de l'api
 const url = "https://api.nasa.gov/planetary/apod?api_key=iho1zSHviwkrk0SPFeoOCiUrBvmUZ6AfVB3HGpHE";
-
+//utilisation de fetch() sur l'api suivi de then() pour convertir le tout en .jso et aller cherche les data se trouvant dedans
 fetch(url).then(response => response.json().then(data => {
+    //récupération de chaque donné et implémentation dans le html
     let divt = document.getElementById("Nasa");
     let img = document.createElement("img");
     let titre = document.createElement("h3");
@@ -26,6 +29,7 @@ fetch(url).then(response => response.json().then(data => {
     img.className = "nasaImg";
     text.innerHTML = data.explanation;
     text.className = "nasaText";
-    console.log(data.url);
+    //affichage du dictionaire se trouvant le .json dans la console
+    console.log(data);
 
 }));
